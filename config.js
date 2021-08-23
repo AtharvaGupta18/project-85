f0fd2a8add28d298147bbd03648861e5b0dbafa9
AtharvaGupta18 <70589193+AtharvaGupta18@users.noreply.github.com>
2021-08-23 21:33:03 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB3ePLaKM7eomLfq6iEvAqvNRB-pyUtb6A",
+  authDomain: "project-85-ef9bc.firebaseapp.com",
+  databaseURL: "https://project-85-ef9bc-default-rtdb.firebaseio.com",
+  projectId: "project-85-ef9bc",
+  storageBucket: "project-85-ef9bc.appspot.com",
+  messagingSenderId: "286584916194",
+  appId: "1:286584916194:web:090f6e0fc520c30ec9f6bb"
+};
